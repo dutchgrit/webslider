0cd94ac5a6db54efca1503cab3b029fe716feea0
Nick <nick@pc.local>
2024-07-22 16:13:38 +0200

    show settings popup button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { PopupComponent } from '../popup/popup.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [PopupComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  showSettings: boolean = false;
 }
