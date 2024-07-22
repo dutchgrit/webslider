1d009c11fcf390b36b2fefb01d82f3811221cbae
Nick <nick@pc.local>
2024-07-22 16:56:29 +0200

    drop down menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,13 +7,33 @@ import { ISlide } from './Slide';
   standalone: true,
   imports: [PopupComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   showSettings: boolean = false;
   showForm: boolean = false;
 
   startSlideShow() {
+    // Implement start slideshow functionality
+    throw new Error('Method not implemented.');
+  }
+
+  downloadSlides() {
+    // Implement download slides functionality
+    throw new Error('Method not implemented.');
+  }
+
+  importSlides(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      // Implement file import functionality
+      console.log('File selected:', file.name);
+    }
+  }
+
+  deleteSlides() {
+    // Implement delete slides functionality
     throw new Error('Method not implemented.');
   }
 }
