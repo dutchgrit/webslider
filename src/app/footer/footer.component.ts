6a9441e7acca4d3599a49d93716fc874222c279d
Nick <nick@pc.local>
2024-07-22 14:00:02 +0200

    basic popup

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
   today: number = new Date().getFullYear();
+  showAboutPopup: boolean = false;
 
   showAbout(event: Event): void {
     event.preventDefault();
-    console.log('Show about popup');
+    this.showAboutPopup = true;
+  }
+
+  closeAbout(): void {
+    this.showAboutPopup = false;
   }
 }
