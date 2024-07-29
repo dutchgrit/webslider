a6878b9eb129ce1e2974870a9b7aa01b1e671330
Nick <nick@pc.local>
2024-07-29 14:35:10 +0200

    hide if slide show is  running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SlideListComponent } from './slide-list/slide-list.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { SlideService } from './slide.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, SlideListComponent, SlideshowComponent],
+  imports: [CommonModule, FooterComponent, HeaderComponent, SlideListComponent, SlideshowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(public slideService: SlideService) {}
+}
