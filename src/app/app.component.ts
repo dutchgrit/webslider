962f6a20f17814e916a8eebd388601140b7b32d2
Nick <nick@pc.local>
2024-07-29 11:35:53 +0200

    use list comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SlideListComponent } from './slide-list/slide-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, SlideListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'web-slide';
-}
+export class AppComponent { }
