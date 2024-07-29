91e2e48ecc18ec0d270cec439fdd04196abe5804
Nick <nick@pc.local>
2024-07-29 13:33:17 +0200

    edit slide

--- src/app/slide-list/slide-list.component.ts
+++ src/app/slide-list/slide-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SlideService } from '../slide.service';
+import { PopupComponent } from '../popup/popup.component';
 import { ISlide } from '../ISlide';
 import { SafeUrlPipe } from './safe-url.pipe';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -9,12 +11,21 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-slide-list',
   standalone: true,
-  imports: [CommonModule, SafeUrlPipe, DragDropModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SafeUrlPipe,
+    DragDropModule,
+    PopupComponent,
+  ],
   templateUrl: './slide-list.component.html',
   styleUrls: ['./slide-list.component.css'],
 })
 export class SlideListComponent implements OnInit {
   slides: ISlide[] = [];
+  showForm: boolean = false;
+  editIndex: number | null = null;
+  newSlide: ISlide = { url: '', slideTime: 0, transition: 'fade' };
 
   constructor(private slideService: SlideService) {}
 
@@ -29,9 +40,19 @@ export class SlideListComponent implements OnInit {
   }
 
   editSlide(index: number): void {
-    const slide = this.slides[index];
-    // Implement the edit functionality here
-    // For example, you can navigate to an edit page or open a modal
-    console.log('Editing slide', index, slide);
+    this.newSlide = { ...this.slides[index] };
+    this.editIndex = index;
+    this.showForm = true;
+  }
+
+  onSubmit(): void {
+    if (this.editIndex !== null) {
+      this.slides[this.editIndex] = { ...this.newSlide };
+    } else {
+      this.slides.push({ ...this.newSlide });
+    }
+    this.showForm = false;
+    this.newSlide = { url: '', slideTime: 0, transition: 'fade' };
+    this.editIndex = null;
   }
 }
