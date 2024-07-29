020953e6c37d954553cbdb82eb048db321aaccdd
Nick <nick@pc.local>
2024-07-29 11:38:20 +0200

    list of slides

--- src/app/slide-list/slide-list.component.ts
+++ src/app/slide-list/slide-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SlideService } from '../slide.service';
+import { ISlide } from '../ISlide';
 
 @Component({
   selector: 'app-slide-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './slide-list.component.html',
   styleUrl: './slide-list.component.css'
 })
 export class SlideListComponent {
+  slides: ISlide[] = [];
 
+  constructor(private slideService: SlideService) {}
+
+  ngOnInit(): void {
+    this.loadSlides();
+  }
+
+  loadSlides(): void {
+    this.slides = this.slideService.getSlides();
+  }
 }
