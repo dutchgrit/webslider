4afe0bc428ec22de46a8fb019edfaacc7b3e9b3b
Nick <nick@pc.local>
2024-07-29 12:34:24 +0200

    drag and drop the order off slides

--- src/app/slide-list/slide-list.component.ts
+++ src/app/slide-list/slide-list.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SlideService } from '../slide.service';
 import { ISlide } from '../ISlide';
 import { SafeUrlPipe } from './safe-url.pipe';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-slide-list',
   standalone: true,
-  imports: [ CommonModule, SafeUrlPipe ],
+  imports: [CommonModule, SafeUrlPipe, DragDropModule],
   templateUrl: './slide-list.component.html',
-  styleUrl: './slide-list.component.css'
+  styleUrls: ['./slide-list.component.css'],
 })
 export class SlideListComponent implements OnInit {
   slides: ISlide[] = [];
@@ -20,5 +22,9 @@ export class SlideListComponent implements OnInit {
     this.slideService.slides$.subscribe((slides) => {
       this.slides = slides;
     });
+  }
+
+  drop(event: CdkDragDrop<ISlide[]>): void {
+    moveItemInArray(this.slides, event.previousIndex, event.currentIndex);
   }
 }
